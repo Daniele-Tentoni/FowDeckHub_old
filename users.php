7502b93f1bca097bd321f94c44174d4136095a5e
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2018-09-18 15:01:50 +0200

    Revert "Alcune modifiche di usabilità"
    This reverts commit eadaf18c387183a6409fb1f584eadd1e33b724be.

--- users.php
+++ users.php
@@ -1,11 +1,5 @@
 <?php
 session_start();
-$conn = new mysqli("localhost", "root", "", "my_fowdeckhub");
-if($conn->connect_error){
-	$error = "Connect Error";
-} else {
-	$error = "Nothing";
-}
 $title = "Users - Administrator - Fow Deck Hub";
 $active_page = 10;
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
