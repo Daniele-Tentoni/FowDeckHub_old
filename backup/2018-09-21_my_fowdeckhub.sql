5eed8a39fd3775cf2915788f55b71dc5af783a58
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2018-09-21 18:01:40 +0200

    Backup del piccolo database.

---
+++ backup/2018-09-21_my_fowdeckhub.sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Creato il: Set 21, 2018 alle 17:58
+-- Versione del server: 10.1.8-MariaDB
+-- Versione PHP: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_fowdeckhub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cards`
+--
+
+CREATE TABLE `cards` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `Set` int(11) NOT NULL,
+  `Number` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Cost` int(11) NOT NULL,
+  `Attribute` int(11) NOT NULL,
+  `Rarity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `composta`
+--
+
+CREATE TABLE `composta` (
+  `Card` int(11) NOT NULL,
+  `Decklist` int(11) NOT NULL,
+  `Deck` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `decklists`
+--
+
+CREATE TABLE `decklists` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `Ruler` int(11) NOT NULL,
+  `Player` varchar(50) COLLATE utf8_bin NOT NULL,
+  `Event` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Style` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `events`
+--
+
+CREATE TABLE `events` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `Location` int(11) NOT NULL,
+  `Nation` int(11) NOT NULL,
+  `Year` int(11) NOT NULL,
+  `Attendance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `userid` int(11) NOT NULL,
+  `dataaccesso` varchar(30) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `roles`
+--
+
+CREATE TABLE `roles` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `UserPower` int(1) NOT NULL DEFAULT '0',
+  `CardPower` int(1) NOT NULL DEFAULT '0',
+  `DecklistPower` int(1) NOT NULL DEFAULT '0',
+  `EventsPower` int(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dump dei dati per la tabella `roles`
+--
+
+INSERT INTO `roles` (`Id`, `Name`, `UserPower`, `CardPower`, `DecklistPower`, `EventsPower`) VALUES
+(1, 'Superuser', 1, 1, 1, 1),
+(2, 'User', 0, 0, 0, 0),
+(3, 'Administrator', 0, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` char(128) COLLATE utf8_bin NOT NULL,
+  `salt` char(128) COLLATE utf8_bin NOT NULL,
+  `registerdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dump dei dati per la tabella `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `salt`, `registerdate`) VALUES
+(1, 'AntonioParolisi', 'daniele.tentoni.1996@gmail.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef', '2018-09-19');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `cards`
+--
+ALTER TABLE `cards`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Id` (`Id`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indici per le tabelle `composta`
+--
+ALTER TABLE `composta`
+  ADD PRIMARY KEY (`Card`,`Decklist`,`Deck`),
+  ADD KEY `Decklist` (`Decklist`);
+
+--
+-- Indici per le tabelle `decklists`
+--
+ALTER TABLE `decklists`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Id` (`Id`),
+  ADD UNIQUE KEY `Name` (`Name`),
+  ADD KEY `Ruler` (`Ruler`),
+  ADD KEY `Event` (`Event`);
+
+--
+-- Indici per le tabelle `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Id` (`Id`),
+  ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indici per le tabelle `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Id` (`Id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `decklists`
+--
+ALTER TABLE `decklists`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `events`
+--
+ALTER TABLE `events`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT per la tabella `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `composta`
+--
+ALTER TABLE `composta`
+  ADD CONSTRAINT `composta_ibfk_1` FOREIGN KEY (`Card`) REFERENCES `cards` (`Id`),
+  ADD CONSTRAINT `composta_ibfk_2` FOREIGN KEY (`Decklist`) REFERENCES `decklists` (`Id`);
+
+--
+-- Limiti per la tabella `decklists`
+--
+ALTER TABLE `decklists`
+  ADD CONSTRAINT `decklists_ibfk_1` FOREIGN KEY (`Ruler`) REFERENCES `cards` (`Id`),
+  ADD CONSTRAINT `decklists_ibfk_2` FOREIGN KEY (`Event`) REFERENCES `events` (`Id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
