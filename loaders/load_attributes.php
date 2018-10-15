1f9b07afb7d374d4e15d07e8ca28bc14a4967734
Daniele Tentoni <daniele.tentoni.1996@gmail.com>
2018-10-15 23:45:47 +0200

    Aggiunti loaders per risorse per aggiunta classi.

---
+++ loaders/load_attributes.php
@@ -0,0 +1,27 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "my_fowdeckhub");
+$msg = array();
+$msg["result"] = true;
+if($conn->connect_error){
+	$msg["error"] = "Connect Error";
+	$msg["result"] = false;
+} else {
+	$query = "SELECT a.Id, a.Name
+				FROM attributes a";
+	$stmt = $conn->prepare($query);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	if($result->num_rows > 0) {
+		$msg["content"] = array();
+		while($row = $result->fetch_assoc()) {
+			$stringa["Id"] = $row["Id"];
+			$stringa["Name"] = $row["Name"];
+			array_push($msg["content"], $stringa);
+		}
+	}
+}
+if(isset($conn)){
+	$conn->close();
+}
+echo json_encode($msg);
+?>
